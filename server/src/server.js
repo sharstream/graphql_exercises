29e689ac411f4078a69bbaa1c637d04dde89c86a
David Perez <kitsunebrave@gmail.com>
2018-10-18 14:05:47 -0400

    run graphiql local

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,7 @@
+import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { makeExecutableSchema } from 'graphql-tools';
 import { graphql } from 'graphql';
 
@@ -20,8 +24,14 @@ const resolvers = {
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const query = process.argv[2];
+const app = express();
 
-graphql(schema, query).then(result => {
-  console.log(JSON.stringify(result, null, 2));
+app.use(cors());
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.listen(4000, () => {
+  console.log(`Go to http://localhost:4000/graphiql to run queries!`);
 });
