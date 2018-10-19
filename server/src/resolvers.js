70cc376521be3f64a82593d3968e58fb4ee6244f
David Perez <kitsunebrave@gmail.com>
2018-10-19 19:36:26 -0400

    code challenge fixed - ratingCount returning value

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,6 +1,12 @@
 import { allBooks } from './book';
 
 const resolvers = {
+  Book: {
+    ratingCount: book => book.rating_count
+    // title: book => {
+    //   return `${book.title} (from resolvers)`;
+    // }
+  },
   Query: {
     books: () => {
       return allBooks();
