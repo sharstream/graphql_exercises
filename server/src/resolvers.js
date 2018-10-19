c5e5eb089142b5183060077c1387c01ac552e5b0
David Perez <kitsunebrave@gmail.com>
2018-10-19 19:56:01 -0400

    call humps to work with camel notation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,7 +2,7 @@ import { allBooks } from './book';
 
 const resolvers = {
   Book: {
-    ratingCount: book => book.rating_count
+    // ratingCount: book => book.rating_count
     // title: book => {
     //   return `${book.title} (from resolvers)`;
     // }
