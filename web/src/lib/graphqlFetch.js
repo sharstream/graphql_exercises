1d92aa54016d34b1842337ab614fb942c0ea3d09
David Perez <kitsunebrave@gmail.com>
2018-10-22 15:19:01 -0400

    suppling extended request option

--- web/src/lib/graphqlFetch.js
+++ web/src/lib/graphqlFetch.js
@@ -1,6 +1,15 @@
 const options = {
-  method: 'POST',
-  headers: {'Content-Type': 'application/json'},
+  // Default options are marked with *
+  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  mode: "cors", // no-cors, cors, *same-origin
+  cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+  credentials: "same-origin", // include, same-origin, *omit
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8'
+    // "Content-Type": "application/x-www-form-urlencoded",
+  },
+  redirect: "follow", // manual, *follow, error
+  referrer: "no-referrer", // no-referrer, *client
 }
 
 export default url => args => {
