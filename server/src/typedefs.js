9a9ddfade1b2392a5094887da2d10ac81b3735b3
David Perez <kitsunebrave@gmail.com>
2018-10-27 14:48:04 -0400

    more docs

--- server/src/typedefs.js
+++ server/src/typedefs.js
@@ -1,16 +1,24 @@
 const typeDefs = `
 schema {
+  # graphql query
   query: Query
+  # graphql server
   mutation: Mutation
 }
 
 # root query for our **Hackbook Server**
 type Query {
+  # books order by rating
   books(orderBy: BooksOrderBy = RATING_DESC): [Book]
+  # reviews order by id
   reviews(orderBy: ReviewOrderBy = ID_DESC): [Review]
+  # list fo users
   users: [User]
+  # a book
   book(id: ID!): Book
+  # search a book
   searchBook(query: String!): [SearchBookResult]
+  # search globally by a book or review or author or user or all
   search(query: String!): [SearchResult]
 }
 
@@ -19,26 +27,39 @@ union SearchResult = Book | Review | Author | User
 
 # search to find books
 type SearchBookResult {
+  # search book result unique id
   id: ID!
+  # search book result title
   title: String
+  # search book result description
   description: String
+  # search boo result retrieve authors array
   authors: [String]
+  # search book result image url
   imageUrl(size: ImageSize = LARGE): String
 }
 
 # mutation to create a Book or add a Review
 type Mutation {
+  # add a new review
   createReview(reviewInput: ReviewInput!): Review
+  # add a new book
   createBook(googleBookId: ID!): Book
 }
 
 # create a review
 input ReviewInput {
+  # book review input unique id
   bookId: ID!
+  # review input rating
   rating: Int!
+  # review input name
   name: String!
+  # review input email
   email: String!
+  # review input title
   title: String
+  # review ionput comment
   comment: String
 }
 
@@ -56,37 +77,61 @@ enum BooksOrderBy {
 
 # book fields
 type Book {
+  # book unique id
   id: ID!
+  # book title
   title: String!
+  # book description
   description: String!
+  # book image url
   imageUrl(size: ImageSize = LARGE): String!
+  # book rating
   rating: Float
+  # book subtitle
   subtitle: String
+  # book rating count
   ratingCount: Int
+  # list of authors by book
   authors: [Author]
+  # list of reviews by book
   reviews: [Review]
 }
 
 # author fields
 type Author {
+  # author unique id
   id: ID!
-  name: String!
+  # author name
+  name: String! @deprecated(reason: "Use \`firstName & lastName\`.")
+  # author first name
+  firstName: String
+  # author last name
+  lastName: String
 }
 
 # review fields
 type Review {
+  # review unique id
   id: ID!
+  # review rating
   rating: Int
+  # review title
   title: String
+  # review comment
   comment: String
+  # a reviewer book
   book: Book
+  # a reviewer user
   user: User
 }
 
 # user fields
 type User {
+  # user unique id
   id: ID!
+  # user name
   name: String!
+  # user image url
   imageUrl(size: Int = 50): String
 }
 
