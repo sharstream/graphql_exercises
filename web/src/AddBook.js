58a088fb7b3950c036005050f7a3eaa01c944e07
David Perez <kitsunebrave@gmail.com>
2018-10-27 11:22:20 -0400

    add book

--- web/src/AddBook.js
+++ web/src/AddBook.js
@@ -1,4 +1,4 @@
-import { pathOr, map } from 'ramda';
+import { pathOr, map, path } from 'ramda';
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { BookSearchForm, BookSearchResults } from './components/Book';
@@ -17,6 +17,14 @@ fragment SearchBook on SearchBookResult {
 query SearchBook($query: String!) {
   searchBook(query: $query) {
     ...SearchBook
+  }
+}
+`;
+
+const createBookMutation = `
+mutation CreateBook($googleBookId: ID!) {
+  createBook(googleBookId: $googleBookId) {
+    id
   }
 }
 `;
@@ -50,8 +58,11 @@ class AddBook extends Component {
   addBook = async googleBookId => {
     try {
       // TODO: add mutation to add book using graphql
-      const redirectBookId = 1;
-      const errors = [];
+      const variables = { googleBookId };
+      const result = await fetch({ query: createBookMutation, variables });
+      const redirectBookId = path(['data', 'createBook', 'id'], result);
+      const errorList = pathOr([], ['errors'], result);
+      const errors = map(error => error.message, errorList);
       this.setState({ redirectBookId, errors });
     } catch (err) {
       this.setState({ errors: [err.message] });
